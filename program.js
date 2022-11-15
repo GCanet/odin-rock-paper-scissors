1239a2324944f9205f32f553746c2108e051ec66
Gerard <kanetpwnz@gmail.com>
2022-11-15 22:42:18 +0100

    first instructions

---
+++ program.js
@@ -0,0 +1,14 @@
+
+
+getComputerChoice () // random return rock - paper - scissors
+
+
+playerSelection // case-insensitive
+computerSelection // response from computerchoice
+
+oneRound () // function that plays one round // return
+
+game () // calls one round * 5 times loops**
+
+
+
