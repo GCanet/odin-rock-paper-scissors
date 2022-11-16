4ffe860c92f8e13c121b528df9dbf17c177d777f
Gerard <kanetpwnz@gmail.com>
2022-11-16 19:18:24 +0100

    working product

--- program.js
+++ program.js
@@ -4,62 +4,55 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3)
   switch (randomNumber) {
     case 0:
-      return 'rock';
+      return `rock`;
     case 1:
-      return 'paper';
+      return `paper`;
     case 2:
-      return 'scissors';
+      return `scissors`;
   }
 }
 
 const computerSelection = getComputerChoice();
 
-console.log(computerSelection);
-
 // playing one round
 
-let playerScore = "0";
-let computerScore = "0";
-let winnerStatus = "";
+let playerScore = `0`;
+let computerScore = `0`;
 
 function playRound(playerSelection, computerSelection) {
     if (
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper")
+        (playerSelection == `paper` && computerSelection == `rock`) ||
+        (playerSelection == `rock` && computerSelection == `scissors`) ||
+        (playerSelection == `scissors` && computerSelection == `paper`)
         ){
-          playerScore ++;
-          winnerStatus = "You won the round.";
+          playerScore++;
         }
     if (
-        (playerSelection == "scissors" && computerSelection == "rock") ||
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors")
+        (playerSelection == `scissors` && computerSelection == `rock`) ||
+        (playerSelection == `rock` && computerSelection == `paper`) ||
+        (playerSelection == `paper` && computerSelection == `scissors`)
         ){
-          computerScore ++;
-          winnerStatus = "Computer wins, you lose the round.";
+          computerScore++;
         }
     if (playerSelection === computerSelection)
         {
-          winnerStatus = "This is a tie round.";
+          playerScore++;
+          computerScore++;
         }
-  return winnerStatus;
 }
 
 // game() function based on loop of 5 playRound() + winner declaration
 
 function game() {
-    
   for (let i = 0; i < 5; i++){
       let playerSelection;
       let computerSelection;
 
-      playerSelection = prompt("Write: Rock, Paper or Scissors").toLowerCase();
+      playerSelection = prompt(`Write Rock, Paper or Scissors:`).toLowerCase();
       computerSelection = getComputerChoice();
 
       console.log(playRound(computerSelection, playerSelection));
   }
-
   if (playerScore > computerScore)
   {
       console.log(`You win! score: ${playerScore} to ${computerScore}`);
