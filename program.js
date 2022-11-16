464b870882d64c149db12e057c73b5986da55b1f
Gerard <kanetpwnz@gmail.com>
2022-11-16 01:48:24 +0100

    pseudocoded

--- program.js
+++ program.js
@@ -1,8 +1,6 @@
-// rock > scissors
-// scissors > paper
-// paper > rock
+// getComputerChoice()
 
-getComputerChoice(Math.random(1,2,3))
+getComputerChoice();
 
 console.log(getComputerChoice());
 
@@ -12,27 +10,36 @@ const playerSelection = prompt("Choose Rock, Paper or Scissors:").toLowerCase();
 
 console.log(playerSelection);
 
+if playerSelection = rock = 1
+if playerSelection = scissors = 2
+if playerSelection = paper = 3
+
 // computerSelection
-// 1 = rock
-// 2 = scissors
-// 3 = paper
+// 1 = rock > 2 = scissors
+// 2 = scissors > 3 = paper
+// 3 = paper > 1 = rock
 
 const computerSelection = getComputerChoice(); ;
+
+if computerSelection = 1 = rock
+if computerSelection = 2 = scissors
+if computerSelection = 3 = paper
 
 console.log(computerSelection);
 
 // oneRound () // function that plays one round // return
 
 function playRound(playerSelection, computerSelection) {
-    if 
-    if
-    else
-    return xxxxxx
-};
+    if (playerSelection = 1) => (computerSelection = 2) return win;
+    if (playerSelection = 2) => (computerSelection = 3) return win;
+    if (playerSelection = 3) => (computerSelection = 1) return win;
+}
+    return win;
+
 
 console.log(function playRound(playerSelection, computerSelection));
 
-// calls one round * 5 times loops**
+// calls one round * 5 times loops
 
 game ();
 
