376b41091a5b9beca3687c6d05d23b57e39a2064
Gerard <kanetpwnz@gmail.com>
2022-11-16 15:10:51 +0100

    code fix

--- program.js
+++ program.js
@@ -1,12 +1,14 @@
 // getting computer choice
 
-const myArray = ["Rock", "Paper", "Scissors"];
+const myArray = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return myArray[~~(Math.random() * myArray.length)];
 }
 
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
 
 // getting player choice
 
@@ -14,48 +16,42 @@ const playerSelection = prompt("Choose Rock, Paper or Scissors:").toLowerCase();
 
 console.log(playerSelection);
 
-// building computer function into variable
-
-const computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
 // playRound() playing one round of player vs computer
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection = "paper") + (computerSelection = "rock")
+    if (playerSelection == "paper" && computerSelection == "rock")
         function win() {
           alert("You won, paper wins rocks");
         }
-    if (playerSelection = "rock") + (computerSelection = "scissors")
+    if (playerSelection == "rock" && computerSelection == "scissors")
         function win() {
           alert("You won, rock wins scissors");
         }
-    if (playerSelection = "scissors") + (computerSelection = "paper")
+    if (playerSelection == "scissors" && computerSelection == "paper")
         function win() {
           alert("You won, scissors wins paper");
         }
-    if (playerSelection = "scissors") + (computerSelection = "rock")
+    if (playerSelection == "scissors" && computerSelection == "rock")
         function lose() {
           alert("You lost, rock wins scissors");
         }
-    if (playerSelection = "rock") + (computerSelection = "paper")
+    if (playerSelection == "rock" && computerSelection == "paper")
         function lose() {
           alert("You lost, paper wins rock");
         }
-    if (playerSelection = "paper") + (computerSelection = "scissors")
+    if (playerSelection == "paper" && computerSelection == "scissors")
         function lose() {
           alert("You lost, scissors wins paper");
         }
-    if (playerSelection = "rock") + (computerSelection = "rock")
+    if (playerSelection == "rock" && computerSelection == "rock")
         function tie() {
           alert("Tie round! No one won");
         }
-    if (playerSelection = "paper") + (computerSelection = "paper")
+    if (playerSelection == "paper" && computerSelection == "paper")
         function tie() {
           alert("Tie round! No one won");
         }
-    if (playerSelection = "scissors") + (computerSelection = "scissors")
+    if (playerSelection == "scissors" && computerSelection == "scissors")
         function tie() {
           alert("Tie round! No one won");
       }
