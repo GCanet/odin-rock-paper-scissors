14093ab7233233195710eeafd5e93b9c3342c0fc
Gerard <kanetpwnz@gmail.com>
2022-11-16 14:42:06 +0100

    game rules added

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-// getComputerChoice()
+// getting computer choice
 
 const myArray = ["Rock", "Paper", "Scissors"];
 
@@ -8,38 +8,59 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
-// playerSelection
+// getting player choice
 
 const playerSelection = prompt("Choose Rock, Paper or Scissors:").toLowerCase();
 
 console.log(playerSelection);
 
-// computerSelection
+// building computer function into variable
 
 const computerSelection = getComputerChoice();
 
 console.log(computerSelection);
 
-// oneRound () // function that plays one round // return
+// playRound() playing one round of player vs computer
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection = 1) + (computerSelection = 2)
+    if (playerSelection = "paper") + (computerSelection = "rock")
         function win() {
-          alert("You won");
+          alert("You won, paper wins rocks");
         }
-    if (playerSelection = 2) + (computerSelection = 3)
+    if (playerSelection = "rock") + (computerSelection = "scissors")
         function win() {
-          alert("You won");
+          alert("You won, rock wins scissors");
         }
-    if (playerSelection = 3) + (computerSelection = 1)
+    if (playerSelection = "scissors") + (computerSelection = "paper")
         function win() {
-          alert("You won");
+          alert("You won, scissors wins paper");
         }
+    if (playerSelection = "scissors") + (computerSelection = "rock")
+        function lose() {
+          alert("You lost, rock wins scissors");
+        }
+    if (playerSelection = "rock") + (computerSelection = "paper")
+        function lose() {
+          alert("You lost, paper wins rock");
+        }
+    if (playerSelection = "paper") + (computerSelection = "scissors")
+        function lose() {
+          alert("You lost, scissors wins paper");
+        }
+    if (playerSelection = "rock") + (computerSelection = "rock")
+        function tie() {
+          alert("Tie round! No one won");
+        }
+    if (playerSelection = "paper") + (computerSelection = "paper")
+        function tie() {
+          alert("Tie round! No one won");
+        }
+    if (playerSelection = "scissors") + (computerSelection = "scissors")
+        function tie() {
+          alert("Tie round! No one won");
+      }
 }
-
-console.log(playRound());
-
-// calls one round * 5 times loops
+// loop to create 1 game based on 5 playRound()
 
 game ();
 
