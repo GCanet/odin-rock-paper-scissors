4dbdce6c33839fc5ff0a257ebc960f3b98a45512
Gerard <kanetpwnz@gmail.com>
2022-11-22 22:41:20 +0100

    working score

--- program.js
+++ program.js
@@ -1,70 +1,48 @@
-buttons.forEach(button =>{
-  button.addEventListener(`click`, function(){
-      playRound(button.id)
-  })
-})
-
-const player = document.querySelector("playerscore");
-player.textContent = `Player Score: ${playerScore}`;
-
-const computer = document.querySelector("computerscore");
-computer.textContent = `Computer Score: ${computerScore}`;
-
-// getting computer choice
+let playerScore = '0';
+let computerScore = '0';
+document.getElementById('playersc').innerHTML = playerScore;
+document.getElementById('computersc').innerHTML = computerScore;
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3)
   switch (randomNumber) {
     case 0:
-      return `rock`;
+      return 'rock';
     case 1:
-      return `paper`;
+      return 'paper';
     case 2:
-      return `scissors`;
+      return 'scissors';
   }
 }
 
-let playerScore = `0`;
-let computerScore = `0`;
+let computerSelection = getComputerChoice();
+let playerSelection = button.id;
 
 function playRound(playerSelection, computerSelection) {
-  let playerSelection = button.id;
-  let computerSelection = getComputerChoice();
-  
-  player.textContent = `You: ${playerScore}`;
-  computer.textContent = `Computer: ${compScore}`;
   if (
-        (playerSelection == `paper` && computerSelection == `rock`) ||
-        (playerSelection == `rock` && computerSelection == `scissors`) ||
-        (playerSelection == `scissors` && computerSelection == `paper`)
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
         ){
           playerScore++;
-          player.textContent = `You: ${playerScore}`;
-          computer.textContent = `Computer: ${compScore}`;
         }
   if (
-        (playerSelection == `scissors` && computerSelection == `rock`) ||
-        (playerSelection == `rock` && computerSelection == `paper`) ||
-        (playerSelection == `paper` && computerSelection == `scissors`)
+        (playerSelection == 'scissors' && computerSelection == 'rock') ||
+        (playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors')
         ){
           computerScore++;
-          player.textContent = `You: ${playerScore}`;
-          computer.textContent = `Computer: ${compScore}`;
         }
   if (playerScore == 5)
         {
-          alert("You are the winner!!");
-          player.textContent = `You: ${playerScore}`;
-          computer.textContent = `Computer: ${compScore}`;
-          playerScore = `0`;
-          computerScore = `0`;
+          alert('You are the winner!!');
+          playerScore = '0';
+          computerScore = '0';
         }
   if (computerScore == 5)
         {
-          alert("Computer Wins!! You lose!!");
-          player.textContent = `You: ${playerScore}`;
-          computer.textContent = `Computer: ${compScore}`;
-          playerScore = `0`;
-          computerScore = `0`;
+          alert('Computer Wins!! You lose!!');
+          playerScore = '0';
+          computerScore = '0';
         }
 }
